7ca562a9d13415314d70efbb2309bff15ca46a7d
Noemie L <831noemie@gmail.com>
2025-01-15 13:42:21 +0100

    telechargement 3 memes fichiers avec filtres

--- skyview_astro.py
+++ skyview_astro.py
@@ -6,33 +6,35 @@ import os
 
 print(SkyView.list_surveys())
 
-target_name = 'M42'
-obs_collection = ['2MASS-H', 'SwiftXRTInt', 'DSS']
+objet = 'M82'
+obs_collection = ['DSS2 Red', 'DSS2 Blue', 'DSS2 IR']
 radius = '0.5 deg'
 
 # examples target : NGC 2024 M42 Andromeda Galaxy Betelgeuse Eta Carinae
 
-def telecharger_fit(target_name, obs_collection):
-    paths = SkyView.get_images(position=target_name, survey=obs_collection)
-    for path in paths:
-        print(' new file:', path)
+def telecharger_fit(objet, obs_collection):
+    paths = SkyView.get_images(position=objet, survey=obs_collection)
+
     for i, img in enumerate(paths):
-        filename = os.path.join('./images', f"image_{obs_collection[i]}.fits")
+        filename = os.path.join('./images', f"{objet}_{obs_collection[i]}.fit")
         img.writeto(filename, overwrite=True)
         print(f"Image sauvegardée : {filename}")
-    SkyView.clear_cache()
+        
+    data = fits.getdata(filename)
+    
+    return data
 
 
-telecharger_fit(target_name, obs_collection)
+
 # DSS-M82
 
 
-# data = fits.getdata(paths[0])
 
-# # Afficher l’image
-# plt.imshow(data, norm=LogNorm(), origin='lower', cmap='viridis')
-# plt.colorbar()
-# plt.show()
+
+# Afficher l’image
+plt.imshow(telecharger_fit(objet, obs_collection), norm=LogNorm(), origin='lower', cmap='viridis')
+plt.colorbar()
+plt.show()
 
 
 SkyView.clear_cache()
