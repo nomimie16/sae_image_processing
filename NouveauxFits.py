5ccde36604008d0b2d2143841e0d98bc34a5520d
Noemie L <831noemie@gmail.com>
2025-01-16 09:01:08 +0100

    - ajout getter et setter
    - changement constructeur -> objet aleatoire si l'user ne choisit rien

--- NouveauxFits.py
+++ NouveauxFits.py
@@ -3,15 +3,34 @@ from astroquery.skyview import SkyView
 from matplotlib.colors import LogNorm
 import matplotlib.pyplot as plt
 import os
+import random
 
 
 class NouveauxFits:
     
     def __init__(self, object):
-        self.object = object
+        # filtres
         self.surveys = ['DSS2 Red', 'DSS2 Blue', 'DSS2 IR']
-
         
+        # si on a un parametre on l'utilise pour choisir l'objet
+        if object:
+            self.object = object
+            
+        #sinon on choisit un objet au hasard   
+        else:
+            liste_fits=['M82','M12','M42','M31','M104', 'Andromeda Galaxy', 'Betelgeuse', 'Eta Carinae']
+            num = random.randint(0, len(liste_fits)-1)
+            self.object = liste_fits[num]
+    
+    def getSurveys(self):
+        return self.surveys
+    
+    def getObject(self):
+        return self.object
+    
+    def setObject(self, object):
+        self.object = object
+            
     def telecharger_fits(self, paths): 
         # paths = SkyView.get_images(position=self.object, surveys=self.surveyss)
 
@@ -24,19 +43,18 @@ class NouveauxFits:
         return data
     
     def fits_existe(self, paths):
-        existe=False;
+        existe :bool =False
         for i, img in enumerate(paths):
             filename = os.path.join('./images', f"{self.object}_{self.surveys[i]}.fit")
             if os.path.exists(filename):
-                print(f"Image existe : {filename}")
                 existe=True
             else:
-                print(f"Image n'existe pas : {filename}")
                 existe=False
     
     def supprimer_fits(self, paths):
         for i, img in enumerate(paths):
             filename = os.path.join('./images', f"{self.object}_{self.surveys[i]}.fit")
+            print(f"Image supprimée : {filename}")
             os.remove(filename)
             print(f"Image supprimée : {filename}")
             
@@ -51,9 +69,11 @@ class NouveauxFits:
         
 if __name__ == '__main__':
     
-    mFits = NouveauxFits('M12')
+    mFits : NouveauxFits = NouveauxFits('M82')
+    # mFits = NouveauxFits()
     
-    paths = SkyView.get_images(position=mFits.object, survey=mFits.surveys)
+    paths : list = SkyView.get_images(position=mFits.object, survey=mFits.surveys)
+    
     if mFits.fits_existe(paths):
         mFits.supprimer_fits()
     else:
